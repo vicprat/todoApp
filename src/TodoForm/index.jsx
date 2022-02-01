7fadf3be4b87c549cd1e0a71dd23a293a79822f4
Victor Prado <vicknan0182@gmail.com>
2022-02-01 12:54:11 -0600

    Aplicación de TODO's hecha en curso de platzi

---
+++ src/TodoForm/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import "./TodoForm.css";
+
+function TodoForm() {
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>Escribe tus nuevas tareas</label>
+      <textarea value={newTodoValue} onChange={onChange} placeholder="" />
+      <div className="TodoForm-buttonContainer">
+        <button
+          className="TodoForm-button TodoForm-button-cancel"
+          type="button"
+          onClick={onCancel}
+        >
+          Cancelar
+        </button>
+        <button className="TodoForm-button TodoForm-button-add" type="submit">
+          Añadir
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export { TodoForm };
